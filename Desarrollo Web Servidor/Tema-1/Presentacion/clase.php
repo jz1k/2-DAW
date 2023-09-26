4cfb281b8821e1d20dd096734dd4e11ab00a5f2a
JZ1K <jzikmusic@gmail.com>
2023-09-26 13:17:18 +0200

    Cliente y servidor

---
+++ Desarrollo Web Servidor/Tema-1/Presentacion/clase.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Relacion de ejercicios 1</title>
+</head>
+<body>
+    echo "<br>Vamos a probar el strspn<br>";
+
+    echo strspn("Este es el directorio de publicacion","Estela");
+</body>
+</html>
